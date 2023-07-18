ce2ce7f4d8983f4df3d00882791c3b05ac85ed52
dkwoods1228 <dkwoods1228@comcast.net>
2023-07-18 01:16:15 -0600

    third save

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,8 +1,15 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -14,15 +21,37 @@ public class MainController implements Initializable {
         System.out.println("Initialized");
     }
 
-    public void mainViewAppointmentsButtonClicked(ActionEvent actionEvent) {
+    public void mainViewAppointmentsButtonClicked(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/Appointment.fxml"));
+        Scene newScene = new Scene(root);
+        Stage goToAppointments = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        goToAppointments.setScene(newScene);
+        goToAppointments.show();
+        goToAppointments.centerOnScreen();
     }
 
-    public void mainViewCustomerButtonClicked(ActionEvent actionEvent) {
+    public void mainViewCustomerButtonClicked(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/Customer.fxml"));
+        Scene newScene = new Scene(root);
+        Stage goToCustomer = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        goToCustomer.setScene(newScene);
+        goToCustomer.show();
+        goToCustomer.centerOnScreen();
     }
 
-    public void mainViewReportsButtonClicked(ActionEvent actionEvent) {
+    public void mainViewReportsButtonClicked(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/Reports.fxml"));
+        Scene newScene = new Scene(root);
+        Stage goToReports = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        goToReports.setScene(newScene);
+        goToReports.show();
+        goToReports.centerOnScreen();
     }
 
     public void mainExitButtonClicked(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION,"Are you sure you would like to exit this program?");
+        alert.showAndWait();
+        Stage exitProgram = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        exitProgram.close();
     }
 }
