ab9f7f0ea94375ede138419a2aef9d1370d55cff
dkwoods1228 <dkwoods1228@comcast.net>
2023-07-19 02:21:54 -0600

    third save

--- src/controller/AppointmentController.java
+++ src/controller/AppointmentController.java
@@ -25,52 +25,92 @@ import java.util.Optional;
 import static main.Timezone.timeAndDateToUTC;
 
 public class AppointmentController {
-    public Button exitButton;
-    public ComboBox<String> updateAppointStartTime;
-    public DatePicker updateAppointStartDate;
-    public DatePicker updateAppointEndDate;
-    public ComboBox<String> updateAppointEndTime;
-    @FXML private TextField updateAppointID;
-    @FXML private TextField updateAppointTitle;
-    @FXML private TextField updateAppointDescription;
-    @FXML private TextField updateAppointLocation;
-    @FXML private ComboBox<String> updateAppointContact;
-    @FXML private ComboBox<String> appointStartTime;
-    @FXML private DatePicker appointStartDate;
-    @FXML private DatePicker appointEndDate;
-    @FXML private ComboBox<String> appointEndTime;
-    @FXML private TextField updateAppointType;
-    @FXML private TextField updateAppointCustomerID;
-    @FXML private TextField updateAppointUserID;
-    @FXML private Button updateSaveAppointmentButton;
-    @FXML private Button cancelButton;
-    @FXML private Button addAppointButton;
-    @FXML private Button deleteButton;
-    @FXML private Button returnToMainMenuButton;
-    @FXML private ToggleGroup toggle;
-    @FXML private RadioButton appointWeekRadioButton;
-    @FXML private RadioButton appointMonthRadioButton;
-    @FXML private RadioButton allAppointRadioButton;
-    @FXML private TableView<Appointment> appointmentTable;
-    @FXML private TableColumn<?,?> appointID;
-    @FXML private TableColumn<?,?> appointTitle;
-    @FXML private TableColumn<?,?> appointDescription;
-    @FXML private TableColumn<?,?> appointLocation;
-    @FXML private TableColumn<?,?> contactID;
-    @FXML private TableColumn<?,?> appointType;
-    @FXML private TableColumn<?,?> appointStart;
-    @FXML private TableColumn<?,?> appointEnd;
-    @FXML private TableColumn<?,?> appointCustomerID;
-    @FXML private TableColumn<?,?> userID;
+    @FXML
+    private Button exitButton;
+    @FXML
+    private ComboBox<String> updateAppointStartTime;
+    @FXML
+    private DatePicker updateAppointStartDate;
+    @FXML
+    private DatePicker updateAppointEndDate;
+    @FXML
+    private ComboBox<String> updateAppointEndTime;
+    @FXML
+    private TextField updateAppointID;
+    @FXML
+    private TextField updateAppointTitle;
+    @FXML
+    private TextField updateAppointDescription;
+    @FXML
+    private TextField updateAppointLocation;
+    @FXML
+    private ComboBox<String> updateAppointContact;
+    @FXML
+    private ComboBox<String> appointStartTime;
+    @FXML
+    private DatePicker appointStartDate;
+    @FXML
+    private DatePicker appointEndDate;
+    @FXML
+    private ComboBox<String> appointEndTime;
+    @FXML
+    private TextField updateAppointType;
+    @FXML
+    private TextField updateAppointCustomerID;
+    @FXML
+    private TextField updateAppointUserID;
+    @FXML
+    private Button updateSaveAppointmentButton;
+    @FXML
+    private Button cancelButton;
+    @FXML
+    private Button addAppointButton;
+    @FXML
+    private Button deleteButton;
+    @FXML
+    private Button returnToMainMenuButton;
+    @FXML
+    private ToggleGroup toggle;
+    @FXML
+    private RadioButton appointWeekRadioButton;
+    @FXML
+    private RadioButton appointMonthRadioButton;
+    @FXML
+    private RadioButton allAppointRadioButton;
+    @FXML
+    private TableView<Appointment> appointmentTable;
+    @FXML
+    private TableColumn<?, ?> appointID;
+    @FXML
+    private TableColumn<?, ?> appointTitle;
+    @FXML
+    private TableColumn<?, ?> appointDescription;
+    @FXML
+    private TableColumn<?, ?> appointLocation;
+    @FXML
+    private TableColumn<?, ?> contactID;
+    @FXML
+    private TableColumn<?, ?> appointType;
+    @FXML
+    private TableColumn<?, ?> appointStart;
+    @FXML
+    private TableColumn<?, ?> appointEnd;
+    @FXML
+    private TableColumn<?, ?> appointCustomerID;
+    @FXML
+    private TableColumn<?, ?> userID;
 
-    @FXML void addAppointButtonClicked(ActionEvent actionEvent) {
+
+    @FXML
+    void addAppointButtonClicked(ActionEvent actionEvent) {
     }
 
-    @FXML void updateSaveButtonClicked(ActionEvent actionEvent) throws IOException {
+    @FXML
+    void updateSaveButtonClicked(ActionEvent actionEvent) throws IOException {
         try {
             Connection connection = DBConnect.openConnection();
 
-            if (!updateAppointTitle.getText().isEmpty() && !updateAppointDescription.getText().isEmpty() && !updateAppointLocation.getText().isEmpty() && !updateAppointType.getText().isEmpty()) && updateAppointStartDate.getValue() != null && updateAppointEndDate.getValue() != null && !updateAppointStartTime.getValue().isEmpty() && !updateAppointEndTime.getValue().isEmpty() && !updateAppointCustomerID.getText().isEmpty()) {
+            if (!updateAppointTitle.getText().isEmpty() && !updateAppointDescription.getText().isEmpty() && !updateAppointLocation.getText().isEmpty() && !updateAppointType.getText().isEmpty() && updateAppointStartDate.getValue() != null && updateAppointEndDate.getValue() != null && !updateAppointStartTime.getValue().isEmpty() && !updateAppointEndTime.getValue().isEmpty() && !updateAppointCustomerID.getText().isEmpty()) {
                 ObservableList<Customer> maintainCustomers = CustomerDAO.getCustomers(connection);
                 ObservableList<Integer> maintainCustomID = FXCollections.observableArrayList();
                 ObservableList<UserDAO> maintainUsers = UserDAO.getUsers();
@@ -88,7 +128,7 @@ public class AppointmentController {
                 LocalTime endLocalTime = LocalTime.parse(updateAppointEndTime.getValue(), timeFormat);
 
                 LocalDateTime startLocalAll = LocalDateTime.of(startLocalDate, startLocalTime);
-                LocalDateTime endLocalAll = LocalDateTime.of(endLocalDate,endLocalTime);
+                LocalDateTime endLocalAll = LocalDateTime.of(endLocalDate, endLocalTime);
 
                 ZonedDateTime startZone = ZonedDateTime.of(startLocalAll, ZoneId.systemDefault());
                 ZonedDateTime endZone = ZonedDateTime.of(endLocalAll, ZoneId.systemDefault());
@@ -106,8 +146,8 @@ public class AppointmentController {
                     outsideBusiness.showAndWait();
                     return;
                 }
-                if (startToEasternTime.toLocalTime().isBefore(LocalTime.of(8,0,0)) || startToEasternTime.toLocalTime().isAfter(LocalTime.of(22,0,0))
-                    || endToEasternTime.toLocalTime().isBefore(LocalTime.of(8,0,0)) || endToEasternTime.toLocalTime().isAfter(LocalTime.of(22,0,0))) {
+                if (startToEasternTime.toLocalTime().isBefore(LocalTime.of(8, 0, 0)) || startToEasternTime.toLocalTime().isAfter(LocalTime.of(22, 0, 0))
+                        || endToEasternTime.toLocalTime().isBefore(LocalTime.of(8, 0, 0)) || endToEasternTime.toLocalTime().isAfter(LocalTime.of(22, 0, 0))) {
                     Alert timeOutsideBusiness = new Alert(Alert.AlertType.ERROR);
                     timeOutsideBusiness.setTitle("Outside Business Operations");
                     timeOutsideBusiness.setContentText("You have selected a time outside of business operations. Business hours are normally 8:00am-10:00pm.");
@@ -132,7 +172,7 @@ public class AppointmentController {
                     sameTimes.showAndWait();
                     return;
                 }
-                for (Appointment appointment: maintainAppointments) {
+                for (Appointment appointment : maintainAppointments) {
                     LocalDateTime startAppointVerify = appointment.getStart();
                     LocalDateTime endAppointVerify = appointment.getEnd();
 
@@ -159,8 +199,8 @@ public class AppointmentController {
                         overlapStartTime.setContentText("The end time of this appointment will overlap with an existing appointment.");
                         overlapStartTime.showAndWait();
                         return;
+                    }
                 }
-            }
                 String timeStart = updateAppointStartTime.getValue();
                 String dateStart = updateAppointStartDate.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
                 String timeEnd = updateAppointEndTime.getValue();
@@ -189,67 +229,67 @@ public class AppointmentController {
 
                 ObservableList<Appointment> listOfAppointments = AppointmentDAO.getAppointments();
                 appointmentTable.setItems(listOfAppointments);
-        }
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+        @FXML void deleteButtonClicked (ActionEvent actionEvent){
+            try {
+                Connection connection = DBConnect.openConnection();
+                int delID = appointmentTable.getSelectionModel().getSelectedItem().getAppointID();
+                String delType = appointmentTable.getSelectionModel().getSelectedItem().getAppointType();
+                Alert delIDAndType = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you would like to delete the appointment containing appointment id of " + delID + " and appointment type of " + delType + "?");
+                Optional<ButtonType> validate = delIDAndType.showAndWait();
+                if (validate.isPresent() && validate.get() == ButtonType.OK) {
+                    AppointmentDAO.deleteAppoint(delID, connection);
 
-        @FXML void deleteButtonClicked(ActionEvent actionEvent) {
-        try {
-            Connection connection = DBConnect.openConnection();
-            int delID = appointmentTable.getSelectionModel().getSelectedItem().getAppointID();
-            String delType = appointmentTable.getSelectionModel().getSelectedItem().getAppointType();
-            Alert delIDAndType = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you would like to delete the appointment containing appointment id of " + delID + " and appointment type of " + delType + "?");
-            Optional<ButtonType> validate = delIDAndType.showAndWait();
-            if (validate.isPresent() && validate.get() == ButtonType.OK) {
-                AppointmentDAO.deleteAppoint(delID, connection);
+                    ObservableList<Appointment> maintainAppointments = AppointmentDAO.getAppointments();
+                    appointmentTable.setItems(maintainAppointments);
+                }
 
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @FXML void cancelButtonClicked (ActionEvent actionEvent){
+        }
+
+        @FXML void returnToMainMenuButtonClicked (ActionEvent actionEvent) throws IOException {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you would like to return to the main menu?");
+            alert.showAndWait();
+            Parent root = FXMLLoader.load(getClass().getResource("/view/Main.fxml"));
+            Scene newScene = new Scene(root);
+            Stage returnToMain = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            returnToMain.setScene(newScene);
+            returnToMain.show();
+            returnToMain.centerOnScreen();
+        }
+
+        @FXML void appointWeekRadioButtonClicked (ActionEvent actionEvent){
+            try {
                 ObservableList<Appointment> maintainAppointments = AppointmentDAO.getAppointments();
-                appointmentTable.setItems(maintainAppointments);
+                ObservableList<Appointment> maintainWeeklyAppointments = FXCollections.observableArrayList();
+                LocalDateTime startWeekNow = LocalDateTime.now().minusWeeks(1);
+                LocalDateTime endWeekNow = LocalDateTime.now().plusWeeks(1);
+
+                if (maintainAppointments != null) {
+                    maintainAppointments.forEach(appointment -> {
+                        if (appointment.getEnd().isBefore(endWeekNow) && appointment.getEnd().isAfter(startWeekNow)) {
+                            maintainWeeklyAppointments.add(appointment);
+                        }
+                        appointmentTable.setItems(maintainWeeklyAppointments);
+                    });
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
 
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
-    }
 
-    @FXML void cancelButtonClicked(ActionEvent actionEvent) {
-    }
-
-    @FXML void returnToMainMenuButtonClicked(ActionEvent actionEvent) throws IOException {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you would like to return to the main menu?");
-        alert.showAndWait();
-        Parent root = FXMLLoader.load(getClass().getResource("/view/Main.fxml"));
-        Scene newScene = new Scene(root);
-        Stage returnToMain = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        returnToMain.setScene(newScene);
-        returnToMain.show();
-        returnToMain.centerOnScreen();
-    }
-
-    @FXML void appointWeekRadioButtonClicked(ActionEvent actionEvent) {
-        try {
-            ObservableList<Appointment> maintainAppointments = AppointmentDAO.getAppointments();
-            ObservableList<Appointment> maintainWeeklyAppointments = FXCollections.observableArrayList();
-            LocalDateTime startWeekNow = LocalDateTime.now().minusWeeks(1);
-            LocalDateTime endWeekNow = LocalDateTime.now().plusWeeks(1);
-
-            if (maintainAppointments != null) {
-                maintainAppointments.forEach(appointment -> {
-                    if (appointment.getEnd().isBefore(endWeekNow) && appointment.getEnd().isAfter(startWeekNow)) {
-                        maintainWeeklyAppointments.add(appointment);
-                    }
-                    appointmentTable.setItems(maintainWeeklyAppointments);
-                });
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-    }
-
-    }
-
-    @FXML void allAppointRadioButtonClicked(ActionEvent actionEvent) {
+        @FXML void allAppointRadioButtonClicked (ActionEvent actionEvent){
             try {
                 ObservableList<Appointment> maintainAppointments = AppointmentDAO.getAppointments();
                 if (maintainAppointments != null)
@@ -261,31 +301,32 @@ public class AppointmentController {
             }
         }
 
-    @FXML void appointMonthRadioButtonClicked(ActionEvent actionEvent) {
-        try {
-            ObservableList<Appointment> maintainAppointments = AppointmentDAO.getAppointments();
-            ObservableList<Appointment> maintainMonthlyAppointments = FXCollections.observableArrayList();
-            LocalDateTime startMonthNow = LocalDateTime.now().minusMonths(1);
-            LocalDateTime endMonthNow = LocalDateTime.now().plusMonths(1);
+        @FXML void appointMonthRadioButtonClicked (ActionEvent actionEvent){
+            try {
+                ObservableList<Appointment> maintainAppointments = AppointmentDAO.getAppointments();
+                ObservableList<Appointment> maintainMonthlyAppointments = FXCollections.observableArrayList();
+                LocalDateTime startMonthNow = LocalDateTime.now().minusMonths(1);
+                LocalDateTime endMonthNow = LocalDateTime.now().plusMonths(1);
 
-            if (maintainAppointments != null) {
-                maintainAppointments.forEach(appointment -> {
-                    if (appointment.getEnd().isBefore(endMonthNow) && appointment.getEnd().isAfter(startMonthNow)) {
-                        maintainMonthlyAppointments.add(appointment);
-                    }
-                    appointmentTable.setItems(maintainMonthlyAppointments);
-                });
+                if (maintainAppointments != null) {
+                    maintainAppointments.forEach(appointment -> {
+                        if (appointment.getEnd().isBefore(endMonthNow) && appointment.getEnd().isAfter(startMonthNow)) {
+                            maintainMonthlyAppointments.add(appointment);
+                        }
+                        appointmentTable.setItems(maintainMonthlyAppointments);
+                    });
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
+        }
 
-        } catch (SQLException e) {
-            e.printStackTrace();
+        public void exitButtonClicked (ActionEvent actionEvent){
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you would like to exit this program?");
+            alert.showAndWait();
+            Stage exitProgram = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            exitProgram.close();
         }
     }
 
-    public void exitButtonClicked(ActionEvent actionEvent) {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION,"Are you sure you would like to exit this program?");
-        alert.showAndWait();
-        Stage exitProgram = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        exitProgram.close();
-    }
-}
